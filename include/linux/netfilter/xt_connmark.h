9b5d046c4f1275fc73ba73a8b34f1524b137dfca
Your Name <ahmedrahmy842@gmail.com>
2017-02-05 21:59:26 +0200

    netfilter: Adding missing header files
    Added xt_mark.h & xt_connmark.h

---
+++ include/linux/netfilter/xt_connmark.h
@@ -0,0 +1,23 @@
+#ifndef _XT_CONNMARK_H
+#define _XT_CONNMARK_H
+
+#include <linux/types.h>
+
+enum {
+XT_CONNMARK_SET = 0,
+XT_CONNMARK_SAVE,
+XT_CONNMARK_RESTORE
+};
+
+struct xt_connmark_tginfo1 {
+__u32 ctmark, ctmask,
+nfmask;
+__u8 mode;
+};
+
+struct xt_connmark_mtinfo1 {
+__u32 mark, mask;
+__u8 invert;
+};
+
+#endif /*_XT_CONNMARK_H*/
